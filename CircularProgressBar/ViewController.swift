67c330d9cc32bc42b59caa27e4406ccc3fdadc1e
Anthony Lee <anthonylee@umass.edu>
2019-01-04 15:16:04 -0500

    Part 2: Progress bar shows the download percentage

--- CircularProgressBar/ViewController.swift
+++ CircularProgressBar/ViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionDownloadDelegate {
+    
+    
     let shapeLayer = CAShapeLayer()
+    
+    let percentageLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Start"
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.textAlignment = .center
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentageLabel.center = view.center
         
         //track Layer
         let trackLayer = CAShapeLayer()
@@ -29,7 +43,7 @@ class ViewController: UIViewController {
         
         view.layer.addSublayer(trackLayer)
         
-        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -(CGFloat.pi/2), endAngle: CGFloat.pi*2, clockwise: true)
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -(CGFloat.pi/2), endAngle: CGFloat.pi*2-(CGFloat.pi/2), clockwise: true)
         shapeLayer.path = circularPath.cgPath
         
         shapeLayer.strokeColor = UIColor.red.cgColor
@@ -45,16 +59,47 @@ class ViewController: UIViewController {
         
     }
     
-    @objc private func handleTap(){
-        print("hl")
+    let urlString = "https://firebasestorage.googleapis.com/v0/b/firestorechat-e64ac.appspot.com/o/intermediate_training_rec.mp4?alt=media&token=e20261d0-7219-49d2-b32d-367e1606500c"
+    
+    fileprivate func beginDownloading(){
+        shapeLayer.strokeEnd = 0
+        let operationQueue = OperationQueue()
+        let urlSession = URLSession(configuration: .default, delegate: self, delegateQueue: operationQueue)
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = urlSession.downloadTask(with: url)
+        task.resume()
+    }
+    
+    fileprivate func animateCircle() {
+        print("animateCircle")
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
         basicAnimation.toValue = 1
-        basicAnimation.duration = 2
+        basicAnimation.duration = 10
         basicAnimation.isRemovedOnCompletion = false
         basicAnimation.fillMode = CAMediaTimingFillMode.forwards
         shapeLayer.add(basicAnimation, forKey: "basic")
     }
+    
+    @objc private func handleTap(){
+        beginDownloading()
+        //animateCircle()
+    }
 
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let percentage = Float(totalBytesWritten)/Float(totalBytesExpectedToWrite)
+        print(percentage)
+        
+        DispatchQueue.main.async {
+            self.percentageLabel.text = "\(Int(percentage*100))%"
+            self.shapeLayer.strokeEnd = CGFloat(percentage)
+        }
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        
+    }
 
 }
 
